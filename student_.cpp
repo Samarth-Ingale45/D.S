54284ab446ff8fa8a8054e5910ee48474cc5a4de
Samarth-Ingale45 <ingalesamarth30@gmail.com>
2026-09-22 19:45:20 +0530

    Create student_.cpp

---
+++ student_.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int rollno;
+    cout<<"Enter The Rollno's of 5 students:- "<<'\n';
+    for(int i=0;i<5;i++){
+        cin>>rollno;
+
+
+    }
+
+        
+    
+    return 0;
+}
